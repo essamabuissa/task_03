863aba8868b0539c3d8d51bad3164e464afa6b88
Essam <essamabuissa@hotmail.com>
2020-02-12 11:41:20 +0200

    Task 3 Done

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,19 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+    "my_list": [
+    {"restaurant_name":"Macdonalds",
+     "food_type" : "Junk Food",
+     },
 
+     {"restaurant_name" : "Buffalo Wings",
+      "food_type" : "Wings",
+      },
+
+      {"restaurant_name" : "Al Quds",
+       "food_type" : "Mansaf For Hamza",
+      },
+    ]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +26,11 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    "my_object":{"restaurant_name": "99 Grill",
+                 "food_type" : "Burgers",
+                 },
 
-    }
+
+                 }
+
     return render(request, 'detail.html', context)
